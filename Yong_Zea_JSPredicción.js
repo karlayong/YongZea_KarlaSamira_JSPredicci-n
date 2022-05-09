d6eac5ac602599c0db772d051b6a3cb787f50f88
¨Karla <karlayongzea@gmail.com>
2022-05-08 19:25:01 -0500

    subiendo JSPredición

---
+++ Yong_Zea_JSPredicción.js
@@ -0,0 +1,38 @@
+/*Predicción 1: Indica "Nací en1980";
+
+function myBirthYearFunc(){
+    console.log("Nací en" + 1980);
+}
+myBirthYear();
+*/
+
+
+/*Predicción 2: "Nací en1980";
+
+var birthYearInput = 1980
+
+function myBirthYearFunc(birthYearInput){
+    console.log("Nací en" + birthYearInput);
+}
+myBirthYearFunc(birthYearInput)*/
+
+
+
+/*Predicción 3: 
+console.log("¡Sumando números!"); = ¡Sumando números!
+console.log("num1 es: " + num1); = num1 es: 10
+console.log("num2 es: " + num2);= num2 es: 20
+var sum = num1 + num2; = 30
+
+
+var num1 = 10;
+var num2 = 20;
+function add(num1, num2){
+    console.log("¡Sumando números!"); //
+    console.log("num1 es: " + num1);
+    console.log("num2 es: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+add(num1,num2)
+ */
